309bd52065dcf689716987a2fcb08d9eac526112
fmauNeko <fmaunier@gmail.com>
2014-11-09 09:56:03 +0000

    Dynamically create broadcaster elements with ThingModel, and using only one global observer, instead of one per element.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,7 +11,31 @@
 })(wrap(document));
 
 TwitchThing = function() {
+  'use strict';
+  
   this.warehouse = new ThingModel.Warehouse();
+
+  this.warehouse.RegisterObserver({
+    New: function(thing) {
+      var broadcasterElement = document.createElement("twitchteam-broadcaster");
+      broadcasterElement.setAttribute("broadcaster", thing.ID);
+      broadcasterElement.setAttribute("id", thing.ID + "Status");
+      broadcasterElement.updateStatus(thing);
+
+      var scaffold = document.getElementsByTagName("twitchteam-scaffold")[0];
+      scaffold.appendChild(broadcasterElement);
+    },
+    Deleted: function(){},
+    Updated: function(thing) {
+      var broadcasterElement = document.getElementById(thing.ID + "Status");
+
+      if(broadcasterElement != null) {
+        broadcasterElement.updateStatus(thing);
+      }
+    },
+    Define: function(){}
+  });
+
   this.client = new ThingModel.WebSockets.Client("TwitchTeam", "ws://" + window.location.hostname + ":8083/", this.warehouse);
 };
 
